4816d22820ef117ec0c34c0dbd4e7a6ade42a2bf
RugbyGuy01 <goodmail@golfpvcc.com>
2023-12-14 20:33:38 -0500

    memory app works & tested removed warnings

--- app/src/main/java/com/golfpvcc/memory/MainActivity.kt
+++ app/src/main/java/com/golfpvcc/memory/MainActivity.kt
@@ -10,10 +10,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.golfpvcc.memory.feature.screen.MemoryScreen
 import com.golfpvcc.memory.feature.screen.MemoryViewModel
 import com.golfpvcc.memory.ui.theme.MemoryTheme
